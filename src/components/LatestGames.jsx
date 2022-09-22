fae488f3aa8f648621dd6d175076333fb42aa122
jeffmikhail <mikhailcruz78@gmail.com>
2022-09-22 12:19:18 +0800

    September 22

--- src/components/LatestGames.jsx
+++ src/components/LatestGames.jsx
@@ -1,129 +1,66 @@
-import React from "react";
-import Carousel from "./Carousel";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import Carousel from './Carousel';
+import { Link } from 'react-router-dom';
+import { getAllGames } from '../service/api';
 
-const LatestGames = () => {
-  const data = [
-    {
-      id: 533,
-      title: "A.V.A Global",
-      thumbnail: "https://www.freetogame.com/g/533/thumbnail.jpg",
-      short_description:
-        "A.V.A is a free-to-play online first-person shooter with multiple game modes, unique customizations, as well as PvP and PvE gameplay.",
-      game_url: "https://www.freetogame.com/open/ava",
-      genre: "MMOFPS",
-      platform: "PC (Windows)",
-      publisher: "NEOWIZ",
-      developer: "NEOWIZ",
-      release_date: "2022-08-24",
-      freetogame_profile_url: "https://www.freetogame.com/ava",
-    },
-    {
-      id: 530,
-      title: "Rumbleverse",
-      thumbnail: "https://www.freetogame.com/g/530/thumbnail.jpg",
-      short_description:
-        "Take 40 people, throw them into a city, give them weapons and let them fight it out. That’s the recipe for Iron Galaxy Studios’ new free-to-play Brawler Royale Rumbleverse.",
-      game_url: "https://www.freetogame.com/open/rumbleverse",
-      genre: "Battle Royale",
-      platform: "PC (Windows)",
-      publisher: "Epic Games Publishing",
-      developer: "Iron Galaxy Studios",
-      release_date: "2022-08-11",
-      freetogame_profile_url: "https://www.freetogame.com/rumbleverse",
-    },
-    {
-      id: 529,
-      title: "Tower of Fantasy",
-      thumbnail: "https://www.freetogame.com/g/529/thumbnail.jpg",
-      short_description:
-        "Tower of Fantasy is a 3D open-world RPG, anime-style sci-fi MMO RPG game with unique characters and beautiful open vistas!",
-      game_url: "https://www.freetogame.com/open/tower-of-fantasy",
-      genre: "MMORPG",
-      platform: "PC (Windows)",
-      publisher: "Level Infinite",
-      developer: "Hotta Studio",
-      release_date: "2022-08-10",
-      freetogame_profile_url: "https://www.freetogame.com/tower-of-fantasy",
-    },
-    {
-      id: 531,
-      title: "Magic Spellslingers",
-      thumbnail: "https://www.freetogame.com/g/531/thumbnail.jpg",
-      short_description:
-        "Magic Spellslingers is the latest entry based on Witzards of the Coast’s popular card game Magic: The Gathering.",
-      game_url: "https://www.freetogame.com/open/magic-spellslingers",
-      genre: "Card Game",
-      platform: "PC (Windows)",
-      publisher: "Wizards of the Coast LLC",
-      developer: "Pipeworks Studios, Seismic Games",
-      release_date: "2022-08-05",
-      freetogame_profile_url: "https://www.freetogame.com/magic-spellslingers",
-    },
-    {
-      id: 534,
-      title: "Lost Light",
-      thumbnail: "https://www.freetogame.com/g/534/thumbnail.jpg",
-      short_description:
-        "A post-apocalyptic shooter built around creating a realistic experience.",
-      game_url: "https://www.freetogame.com/open/lost-light",
-      genre: "Shooter",
-      platform: "PC (Windows)",
-      publisher: "NetEase Games",
-      developer: "NetEase Games",
-      release_date: "2022-08-04",
-      freetogame_profile_url: "https://www.freetogame.com/lost-light",
-    },
-  ];
-  return (
-    <div className="container mx-auto py-20 px-10">
-      <div>
-        <p className="nunito text-xl text-white">Latest Game Releases</p>
-        <p className="halant text-3xl md:text-4xl lg:text-5xl font-semibold text-white">
-          TOP NEW GAMES RELEASED <span className="text-[#DC3D4B]">2022</span>
-        </p>
-      </div>
-      <div>
-        <Carousel>
-          {data &&
-            data.map((item, index) => {
-              return (
-                <div className="xl:w-1/4 md:w-1/2 p-4" key={index}>
-                  <div className="bg-[#201B1B] rounded-lg">
-                    <img
-                      className="h-40 w-full object-cover object-center mb-6"
-                      src={item.thumbnail}
-                      alt="content"
-                    ></img>
-                    <div className="px-6 py-5">
-                      <h3 className="halant text-white text-xl font-semibold">
-                        {item.title}
-                      </h3>
-                      <h2 className="halant text-white my-2">
-                        Release Date:
-                        <span className="text-[#DC3D4B] ml-1">
-                          {item.release_date}
-                        </span>
-                      </h2>
-                      <div className="flex justify-end">
-                        <button className="nunito px-4 py-1 bg-[#747474] text-white text-sm rounded-full">
-                          {item.genre}
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-        </Carousel>
-        <div className="text-center mt-12">
-          <button className="bg-[#DC3D4B] hover:bg-gray-800 hover:border-white hover:border text-white py-1 px-4 rounded mt-2">
-            <Link to="/games">See More</Link>
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+const AllGames = () => {
+	const [games, setGames] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	const getGamesList = async () => {
+		setIsLoading(true);
+		setTimeout(async () => {
+			const result = await getAllGames();
+			setGames(result.data);
+			setIsLoading(false);
+		}, 500);
+	};
+
+	useEffect(() => {
+		getGamesList();
+	}, []);
+
+	return (
+		<div className='container mx-auto py-20 px-10'>
+			<div>
+				<p className='nunito text-xl text-white'>Latest Game Releases</p>
+				<p className='halant text-3xl md:text-4xl lg:text-5xl font-semibold text-white mb-3'>
+					TOP NEW GAMES RELEASED <span className='text-[#DC3D4B]'>2022</span>
+				</p>
+			</div>
+			<div>
+				<Carousel>
+					{games &&
+						!isLoading &&
+						games.slice(0, 6).map((game, index) => {
+							return (
+								<div className='xl:w-1/4 md:w-1/2 p-4' key={index}>
+									<div className='bg-[#201B1B] rounded-lg'>
+										<img className='h-40 w-full object-cover object-center mb-6' src={game.thumbnail} alt='content'></img>
+										<div className='px-6 py-5'>
+											<h3 className='halant text-white text-xl font-semibold'>{game.title}</h3>
+											<h2 className='halant text-white my-2'>
+												Release Date:
+												<span className='text-[#DC3D4B] ml-1'>{game.release_date}</span>
+											</h2>
+											<div className='flex justify-end'>
+												<button className='nunito px-4 py-1 bg-[#747474] text-white text-sm rounded-full'>{game.genre}</button>
+											</div>
+										</div>
+									</div>
+								</div>
+							);
+						})}
+				</Carousel>
+				<div className='text-center mt-12'>
+					<button className='bg-[#DC3D4B] hover:bg-gray-800 hover:border-white hover:border text-white py-1 px-4 rounded mt-2'>
+						<Link to='/games'>See More</Link>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
-export default LatestGames;
+export default AllGames;
--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,92 +1,78 @@
-import { SpinnerDotted } from "spinners-react";
-import { useState, useEffect } from "react";
-import { getAllNews } from "../service/api";
-import { Link } from "react-router-dom";
+import { SpinnerDotted } from 'spinners-react';
+import { useState, useEffect } from 'react';
+import { getAllNews } from '../service/api';
+import { Link } from 'react-router-dom';
 
 const AllNews = () => {
-  const [news, setNews] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+	const [news, setNews] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 
-  const getNewsList = async () => {
-    setIsLoading(true);
-    setTimeout(async () => {
-      const result = await getAllNews();
-      setNews(result.data);
-      setIsLoading(false);
-    }, 500);
-  };
+	const getNewsList = async () => {
+		setIsLoading(true);
+		setTimeout(async () => {
+			const result = await getAllNews();
+			setNews(result.data);
+			setIsLoading(false);
+		}, 500);
+	};
 
-  useEffect(() => {
-    getNewsList();
-  }, []);
+	useEffect(() => {
+		getNewsList();
+	}, []);
 
-  return (
-    <div className="bg-[#261F1F]">
-      <div className="container mx-auto">
-        <h5 className="text-4xl halant text-6xl text-center text-white my-20 underline">
-          LATEST NEWS
-        </h5>
-        {isLoading && (
-          <div className="mx-auto">
-            <SpinnerDotted
-              size="10%"
-              color="#DC3D4B"
-              className="mx-auto mb-10"
-            />
-          </div>
-        )}
-        <div className="flex flex-wrap gap-8 bg-[#281D1D] halant text-white pb-40">
-          {news &&
-            !isLoading &&
-            news.map((news, index) => {
-              const { title, image, date, description, link } = news;
+	return (
+		<div className='bg-[#261F1F]'>
+			<div className='container mx-auto'>
+				<h5 className=' halant text-6xl text-center text-white my-20 underline'>LATEST NEWS</h5>
+				{isLoading && (
+					<div className='mx-auto'>
+						<SpinnerDotted size='10%' color='#DC3D4B' className='mx-auto mb-10' />
+					</div>
+				)}
+				<div className='flex flex-wrap gap-8 bg-[#281D1D] halant text-white pb-40'>
+					{news &&
+						!isLoading &&
+						news.slice(0, 3).map((news, index) => {
+							const { title, image, date, description, link } = news;
 
-              return (
-                <div
-                  className={`flex flex-col lg:flex-row shadow-lg w-[100%] py-5
-				  ${index % 2 !== 0 ? "lg:flex-row-reverse" : ""}`}
-                  key={index}
-                >
-                  <div>
-                    <img
-                      className="min-w-[500px] max-w-[500px] max-h-[250px]"
-                      src={image}
-                      alt={title}
-                    />
-                  </div>
-                  <div
-                    className={`p-3 ${index % 2 !== 0 ? "lg:text-right" : ""}`}
-                  >
-                    <div className="mb-2">
-                      <h3 className="font-bold text-xl uppercase underline tracking-wide">
-                        <a href={link} target="_blank" rel="noreferrer">
-                          {title}
-                        </a>
-                      </h3>
-                    </div>
-                    <div className="mb-2">
-                      <p>
-                        <span className="nunito text-xs px-2">{date}</span>
-                      </p>
-                    </div>
-                    <div className="mb-2">
-                      <p>
-                        <span className="nunito text-sm px-2">
-                          {description}
-                        </span>
-                      </p>
-                    </div>
-                    <button className="bg-[#DC3D4B] hover:bg-gray-800 hover:border-white hover:border text-white py-1 px-4 rounded mt-4">
-                      <Link to="/news">See More</Link>
-                    </button>
-                  </div>
-                </div>
-              );
-            })}
-        </div>
-      </div>
-    </div>
-  );
+							return (
+								<div
+									className={`flex flex-col lg:flex-row shadow-lg w-[100%] py-5
+				      ${index % 2 !== 0 ? 'lg:flex-row-reverse' : ''}`}
+									key={index}
+								>
+									<div>
+										<img className='min-w-[500px] max-w-[500px] max-h-[250px]' src={image} alt={title} />
+									</div>
+									<div className={`p-3 ${index % 2 !== 0 ? 'lg:text-right' : ''}`}>
+										<div className='mb-2'>
+											<h3 className='font-bold text-xl uppercase underline tracking-wide'>
+												<a href={link} target='_blank' rel='noreferrer'>
+													{title}
+												</a>
+											</h3>
+										</div>
+										<div className='mb-2'>
+											<p>
+												<span className='nunito text-xs px-2'>{date}</span>
+											</p>
+										</div>
+										<div className='mb-2'>
+											<p>
+												<span className='nunito text-sm px-2'>{description}</span>
+											</p>
+										</div>
+										<button className='bg-[#DC3D4B] hover:bg-gray-800 hover:border-white hover:border text-white py-1 px-4 rounded mt-4'>
+											<Link to='/news'>See More</Link>
+										</button>
+									</div>
+								</div>
+							);
+						})}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default AllNews;
