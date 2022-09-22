c797865711818fac36377d380327a4b0bd289f05
jeffmikhail <mikhailcruz78@gmail.com>
2022-09-22 21:22:52 +0800

    edited favicon and did some destructuring for cleaner code

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,69 +1,67 @@
 const categories = [
-  "3d",
-  "2d",
-  "action",
-  "action-rpg",
-  "anime",
-  "battle-royale",
-  "card",
-  "fantasy",
-  "fighting",
-  "flight",
-  "mmorpg",
-  "shooter",
-  "strategy",
-  "moba",
-  "racing",
-  "sports",
-  "social",
-  "sandbox",
-  "open-world",
-  "survival",
-  "pvp",
-  "pve",
-  "pixel",
-  "voxel",
-  "zombie",
-  "turn-based",
-  "first-person",
-  "third-Person",
-  "top-down",
-  "tank",
-  "space",
-  "sailing",
-  "side-scroller",
-  "superhero",
-  "permadeath",
-  "mmo",
-  "mmofps",
-  "mmotps",
-  "sci-fi",
-  "military",
-  "martial-arts",
-  "low-spec",
-  "tower-defense",
-  "horror",
-  "mmorts",
+	'3d',
+	'2d',
+	'action',
+	'action-rpg',
+	'anime',
+	'battle-royale',
+	'card',
+	'fantasy',
+	'fighting',
+	'flight',
+	'mmorpg',
+	'shooter',
+	'strategy',
+	'moba',
+	'racing',
+	'sports',
+	'social',
+	'sandbox',
+	'open-world',
+	'survival',
+	'pvp',
+	'pve',
+	'pixel',
+	'voxel',
+	'zombie',
+	'turn-based',
+	'first-person',
+	'third-Person',
+	'top-down',
+	'tank',
+	'space',
+	'sailing',
+	'side-scroller',
+	'superhero',
+	'permadeath',
+	'mmo',
+	'mmofps',
+	'mmotps',
+	'sci-fi',
+	'military',
+	'martial-arts',
+	'low-spec',
+	'tower-defense',
+	'horror',
+	'mmorts',
 ];
 
 const Dropdown = (props) => {
-  return (
-    <div>
-      <select
-        className="nunito text-sm uppercase rounded-lg"
-        value={props.value}
-        onChange={props.onChange}
-      >
-        {categories.sort().map((item, index) => {
-          return (
-            <option key={index} value={item}>
-              {item}
-            </option>
-          );
-        })}
-      </select>
-    </div>
-  );
+	const { value, onChange } = props;
+
+	return (
+		<div>
+			<select className='nunito text-sm uppercase rounded-lg' value={value} onChange={onChange}>
+				{categories.sort().map((item, index) => {
+					return (
+						<option key={index} value={item}>
+							{item}
+						</option>
+					);
+				})}
+			</select>
+		</div>
+	);
 };
 
 export default Dropdown;
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,91 +1,74 @@
-import { SpinnerDotted } from "spinners-react";
-import { useState, useEffect } from "react";
-import { getAllNews } from "../service/api";
-import Footer from "./Footer";
-import NaviBar from "./NaviBar";
+import { SpinnerDotted } from 'spinners-react';
+import { useState, useEffect } from 'react';
+import { getAllNews } from '../service/api';
+import Footer from './Footer';
+import NaviBar from './NaviBar';
 
 const AllNews = () => {
-  const [news, setNews] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+	const [news, setNews] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 
-  const getNewsList = async () => {
-    setIsLoading(true);
-    setTimeout(async () => {
-      const result = await getAllNews();
-      setNews(result.data);
-      setIsLoading(false);
-    }, 500);
-  };
+	const getNewsList = async () => {
+		setIsLoading(true);
+		setTimeout(async () => {
+			const result = await getAllNews();
+			setNews(result.data);
+			setIsLoading(false);
+		}, 1000);
+	};
 
-  useEffect(() => {
-    getNewsList();
-  }, []);
+	useEffect(() => {
+		getNewsList();
+	}, []);
 
-  return (
-    <div className="bg-[#291D24]">
-      <NaviBar title="News" />
-      <div className="container mx-auto">
-        <h5 className="halant text-5xl text-left font-bold text-white my-10">
-          LATEST NEWS
-        </h5>
-        {isLoading && (
-          <div className="mx-auto">
-            <SpinnerDotted
-              size="20%"
-              color="#DC3D4B"
-              className="mx-auto mb-10"
-            />
-          </div>
-        )}
-        <div className="flex flex-wrap gap-8 bg-[#281D1D] halant text-white mb-20">
-          {news &&
-            !isLoading &&
-            news.map((news, index) => {
-              const { title, image, date, description, link } = news;
+	return (
+		<div className='bg-[#291D24]'>
+			<NaviBar title='News' />
+			<div className='container mx-auto'>
+				<h5 className='halant text-5xl text-left font-bold text-white my-10'>LATEST NEWS</h5>
+				{isLoading && (
+					<div className='mx-auto'>
+						<SpinnerDotted size='20%' color='#DC3D4B' className='mx-auto mb-10' />
+					</div>
+				)}
+				<div className='flex flex-wrap gap-8 bg-[#281D1D] halant text-white mb-20'>
+					{news &&
+						!isLoading &&
+						news.map((news, index) => {
+							const { title, image, date, description, link } = news;
 
-              return (
-                <div
-                  className="flex flex-col lg:flex-row shadow-lg w-[100%] lg:min-h-[250px]"
-                  key={index}
-                >
-                  <div>
-                    <img
-                      className="min-w-[500px] max-w-[500px] max-h-[250px]"
-                      src={image}
-                      alt={title}
-                    />
-                  </div>
-                  <div className="p-3">
-                    <div className="mb-2">
-                      <h3 className="font-bold text-xl uppercase underline tracking-wide">
-                        <a href={link} target="_blank" rel="noreferrer">
-                          {title}
-                        </a>
-                      </h3>
-                    </div>
-                    <div className="mb-2">
-                      <p>
-                        <span className="nunito text-gray-400 text-xs px-2">
-                          {date}
-                        </span>
-                      </p>
-                    </div>
-                    <div className="mb-2">
-                      <p>
-                        <span className="nunito font-thin text-lg px-2">
-                          {description}
-                        </span>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-        </div>
-      </div>
-      <Footer title="News" />
-    </div>
-  );
+							return (
+								<div className='flex flex-col lg:flex-row shadow-lg w-[100%] lg:min-h-[250px]' key={index}>
+									<div>
+										<img className='min-w-[500px] max-w-[500px] max-h-[250px]' src={image} alt={title} />
+									</div>
+									<div className='p-3'>
+										<div className='mb-2'>
+											<h3 className='font-bold text-xl uppercase underline tracking-wide'>
+												<a href={link} target='_blank' rel='noreferrer'>
+													{title}
+												</a>
+											</h3>
+										</div>
+										<div className='mb-2'>
+											<p>
+												<span className='nunito text-gray-400 text-xs px-2'>{date}</span>
+											</p>
+										</div>
+										<div className='mb-2'>
+											<p>
+												<span className='nunito font-thin text-lg px-2'>{description}</span>
+											</p>
+										</div>
+									</div>
+								</div>
+							);
+						})}
+				</div>
+			</div>
+			<Footer title='News' />
+		</div>
+	);
 };
 
 export default AllNews;
